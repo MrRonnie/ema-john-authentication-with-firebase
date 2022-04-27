4ae4b77b6954d331be1384dd83ab22ad678e45fd
MrRonnie <ronymaza55@gmail.com>
2022-04-28 03:22:56 +0600

    Set Page and Size state and send search query to server

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,9 +7,17 @@ import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
-  const [products, setProducts] = useProducts();
   const [cart, setCart] = useState([]);
   const [pageCount, setPageCount] = useState();
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/product?page=${page}&size${size}`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
   //   pagination
   useEffect(() => {
@@ -67,8 +75,22 @@ const Shop = () => {
         ))}
         <div className="pagination">
           {[...Array(pageCount).keys()].map((number) => (
-            <button>{number + 1}</button>
+            <button
+              className={page === number ? "selected" : ""}
+              onClick={() => setPage(number)}
+            >
+              {number + 1}
+            </button>
           ))}
+
+          <select onChange={(e) => setSize(e.target.value)}>
+            <option value="5">5 </option>
+            <option value="10" selected>
+              10
+            </option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+          </select>
         </div>
       </div>
       <div className="cart-container">
